42a3b18903d2e035b3df94631adc10df1878040f
ilyailyushonok <ilyushonok2014@mail.ru>
2025-01-28 21:08:21 +0300

    first 2

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import './App.css';
+import styled from 'styled-components';
+
+
+function App() {
+    return (
+        <div className="App">
+            <Title>Styled-components <span>.attrs</span> method</Title>
+            <Form>
+                <Field type="text" />
+                <Field type="password" />
+                <Checkbox/>
+                <Checkbox title={'INCUBATOR'}/>
+                <Checkbox/>
+
+            </Form>
+        </div>
+    );
+}
+
+export default App;
+
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    margin-bottom: 32px;
+    span {
+        color: #e91e63;
+    }
+`;
+
+const Form = styled.form`
+  width: 100%;
+  max-width: 500px;
+`;
+
+const Field = styled.input`
+  padding: 5px 15px;
+  margin: 10px 0;
+  width: 100%;
+  font-size: 1rem;
+`;
+
+const Checkbox = styled.input.attrs((props)=>{
+    // console.log(props.title)
+    const {title}=props;
+    return {
+        type: 'checkbox',
+        title:title||'Super Checkbox',
+    }
+})`
+`
