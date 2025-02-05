d37073b203be9a8cf269c34c7212e8cfcdba238a
ilya ilyushonok <ilyushonok2014@mail.ru>
2025-02-05 19:12:33 +0300

    add font (test)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {StrictMode} from "react";
+import {GlobalStyle} from "./styles/GlobalStyle.tsx";
 
 createRoot(document.getElementById('root')!).render(
-
-    <App />
-
+    <StrictMode>
+        <GlobalStyle/>
+        <App/>
+    </StrictMode>
 )
